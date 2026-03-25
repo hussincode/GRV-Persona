abf8f03c2cd98cd2133118e77b79cb8f150f9789
Hussin Heshaam <hussinhesham2009@gmail.com>
2026-03-25 18:48:43 +0200

    000

--- grv-website/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { Scanline, Navbar }  from "./Navbar";
-export { Hero }              from "./Hero";
-export { About }             from "./About";
-export { Courses }           from "./Courses";
-export { Timeline }          from "./Timeline";
-export { HighBoard }         from "./HighBoard";
-export { Join, Footer }      from "./JoinFooter";
